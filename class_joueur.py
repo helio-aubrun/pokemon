00f09c88e0e75c7a4cc4045b4fb4e50a6e5740f8
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-01-12 15:04:52 +0100

    Update class_joueur.py

--- class_joueur.py
+++ class_joueur.py
@@ -7,8 +7,24 @@ class Joueur :
     def get_list_pkm (self, id) :
         return self.__list_pkm [str (id)]
     
+    #echange entre le sac du joueur et du pc id_sac = le pokemon a anlever id_pc = le pokemon a prendre
+    def chengement (self, id_sac, id_pc) :
+        import json
+        #chargement du contenu du sac et du pc 
+        with open ("list_pkm_joueur.json", "r") as sac, open ("pc_joueur.json", "r") as pc:
+            list_sac = json.load (sac)
+            list_pc = json.load (pc)
 
-    def capture (self, id) :
+        #changement du contenue
+        with open ("list_pkm_joueur.json", "w") as sac, open ("pc_joueur.json", "w") as pc:
+            temp = list_sac [str (id_sac)]
+            list_sac [str (id_sac)] = list_pc [str (id_pc)]
+            list_pc [str (id_pc)] = temp
+            json.dump (list_sac, sac)
+            json.dump (list_pc, pc)
+
+    #capture d'un pokemon id du pokemon capturer
+    def capture (self, id ) :
         import json
         last_key = list(self.__list_pkm.keys())[-1]
         #test de limit du sac dujoueur
@@ -36,11 +52,3 @@ class Joueur :
                     nouveau_pc = {"1" : list_pokedex [str (id)]}
                     json.dump (nouveau_pc, pc)
 
-
-
-
-
-
-    
-test = Joueur ()
-print (test.get_list_pkm (2))
