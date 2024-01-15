9e5427b0c0400251fbee09db09a45ac05a7afce3
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-01-15 09:18:28 +0100

    Add files via upload

---
+++ 4g tileset_interieur.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="4g tileset_interieur" tilewidth="32" tileheight="32" tilecount="632" columns="8">
+ <image source="4g tileset_interieur.png" trans="ff00ff" width="256" height="2532"/>
+</tileset>
